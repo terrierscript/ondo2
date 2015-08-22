a44ba9cfe7d3bfbb3f6d4692d94f4890cc691968
inuscript <inuscript@gmail.com>
2015-08-22 13:27:48 +0900

    mackrel direct

--- pi/mackrel.js
+++ pi/mackrel.js
@@ -1,9 +1,10 @@
 var cache = require("./cache/load")
+var ondo = require("./lib/ondo")
 // var ondo = function(cb){
 //   cb(null, 25.4)
 // }
 
-cache(function(err, degree, time){
+ondo(function(err, degree, time){
   var time = Math.ceil(new Date(time).getTime() / 1000)
   console.log(["degree", degree, time].join("\t"))
   process.exit(0)
