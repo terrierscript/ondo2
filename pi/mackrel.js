b51c675bb99778df6078b9bd91101ec72ea3fb0a
inuscript <inuscript@gmail.com>
2015-08-22 12:00:57 +0900

    fix

--- pi/mackrel.js
+++ pi/mackrel.js
@@ -2,10 +2,8 @@ var ondo = require("./lib/ondo")
 // var ondo = function(cb){
 //   cb(null, 25.4)
 // }
-setTimeout(function()
-  ondo(function(err, c){
-    var time = Math.ceil(new Date().getTime() / 1000)
-    console.log(["degree", c, time].join("\t"))
-    process.exit(0)
-  })
-} 30 * 1000)
+ondo(function(err, c){
+  var time = Math.ceil(new Date().getTime() / 1000)
+  console.log(["degree", c, time].join("\t"))
+  process.exit(0)
+})
