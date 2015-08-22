0d0688fbc264daaca4290cd9814a8fbb07f699ec
inuscript <inuscript@gmail.com>
2015-08-22 12:30:00 +0900

    fix mackrel

--- pi/cache/load.js
+++ pi/cache/load.js
@@ -1,8 +1,9 @@
 
 module.exports = function(cb){
   try{
-    var cache = require("../cache.json")
-    cb(cache.degree, cache.time)
+    var cache = require("../../cache.json")
+    cb(null, cache.degree, cache.time)
   }catch(e){
+    cb(e)
   }
 }
--- pi/mackrel.js
+++ pi/mackrel.js
@@ -1,11 +1,10 @@
-var ondo = require("./lib/ondo")
-var cache = require("./cache/load")()
+var cache = require("./cache/load")
 // var ondo = function(cb){
 //   cb(null, 25.4)
 // }
 
-var log = function(degree, time){
+cache(function(err, degree, time){
   var time = Math.ceil(new Date(time).getTime() / 1000)
   console.log(["degree", degree, time].join("\t"))
-}
-log(cache.degree, cache.time)
+  process.exit(0)
+})
