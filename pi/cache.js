647f704d17486420279fd8429a3c23ed4b8be725
inuscript <inuscript@gmail.com>
2015-08-22 12:03:59 +0900

    dump

--- pi/cache.js
+++ pi/cache.js
@@ -1,15 +1,16 @@
 var ondo = require("./lib/ondo")
 var fs = require("fs")
 var cache = "./cache.json"
-var ondo = function(cb){
-  cb(null, 25.4)
-}
+// var ondo = function(cb){
+//   cb(null, 25.4)
+// }
 ondo(function(err, c){
   var time = Math.ceil(new Date().getTime() / 1000)
-  fs.writeFileSync(cache, JSON.stringify({
+  var data = JSON.stringify({
     degree: c,
     time: time
-  }))
-  // console.log(["degree", c, time].join("\t"))
-  process.exit(0)
+  })
+  fs.writeFile(cache, data, function(){
+    process.exit(0)
+  })
 })
